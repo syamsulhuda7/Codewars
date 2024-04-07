5b1abc754f57d5b7032855fb189a80deb8c894be
Syamsul Huda Harisul Muslimin <syamsulhuda748@gmail.com>
2024-04-08 06:22:43 +0700

    add Complementary DNA

---
+++ ComplementaryDNA-[STRINGS-FUNDAMENTALS]-7kyu/index.js
@@ -0,0 +1,19 @@
+// Instructions
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+// Example: (input --> output)
+// "ATTGC" --> "TAACG"
+// "GTAT" --> "CATA"
+
+// Solution
+function dnaStrand(dna){
+    return dna.split('').map(x => {
+      if (x === 'A') {
+        return 'T'
+      } else if (x === 'T') {
+        return 'A'
+      } else if (x === 'G') {
+        return 'C'
+      } else { return 'G'}
+    }).join('')
+  }
